e5179c6b580b56a00e2bf43f7e8a6abdecf47182
taahejac <jan.herren@swisscom.com>
2020-08-31 11:50:02 +0200

    ini

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,18 +1,10 @@
 export default {
   name: 'Home',
-  methods: {
-    fireBasePostExample() {
-      this.$firestore
-        .collection('ColectionName')
-        .doc()
-        .set({
-          //data that should go into the collection
-        })
-        .then(() => {})
-        .catch((error) => {
-          // eslint-disable-next-line
-          console.log(error);
-        });
-    },
+  data() {
+    return {
+      test: 'dies ist ein test',
+    };
   },
+  methods: {
+    // your methods here
 };
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <h1>Libary</h1>
+    <!-- your html code here -->
   </div>
 </template>
 <script src="./Home.js"></script>
