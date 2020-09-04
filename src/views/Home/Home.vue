357db7e8f1730a7aea281472af4bdcf6a80214d8
Islami Dioneta <Dioneta.Islami@swisscom.com>
2020-09-04 11:25:53 +0200

    Done

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="container">
-    <h1>Libary</h1>
-    <!-- your html code here -->
+    <h1>Library</h1>
+    <h2>Genre</h2>
+    <h3>Fantasy</h3>
+    <h3>Action</h3>
+    <h3>Romantic</h3>
+    <h3>Comedy</h3>
+    <h3>Teens</h3>
+    <p>This is a virtual form of a traditional library. This Library focuses on movie genres.</p>
   </div>
 </template>
 <script src="./Home.js"></script>
