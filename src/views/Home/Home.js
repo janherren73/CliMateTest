c9cc1710f9a4117ab1a8c3e7a5c4b022d4096748
taahejac <jan.herren@swisscom.com>
2020-09-10 15:54:17 +0200

    tipps

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -3,38 +3,11 @@ export default {
   data() {
     return {
       test: 'dies ist ein test',
+      navOpen: false,
     };
   },
   methods: {
     // your methods here
+    openTab() {},
   },
 };
-function openTab(index)
-{
-  var element = document.getElementsByTagName('div');
-  var name = "";
-  var obj;
-
-  for (var i = 0; i < element.length; i++)
-  {
-    name = element[i].id;
-    if (name.substr(0,8) == 'tab_box_')
-    {
-      obj = document.getElementById(name);
-
-      obj.hidden = true;
-
-    }
-    if (name.substr(0,8) == 'tab_top_')
-    {
-      obj = document.getElementById(name);
-      obj.setAttribute('class','tab_top_passive');
-    }
-
-  }
-  var tab = document.getElementById("tab_box_"+index);
-  tab.hidden = false;
-  tab = document.getElementById("tab_top_"+index);
-  tab.setAttribute('class','tab_top_active');
-}
-
