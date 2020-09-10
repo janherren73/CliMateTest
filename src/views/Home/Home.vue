e865a65ba4b321d049fd2edb260b1a24ca47094f
Islami Dioneta <Dioneta.Islami@swisscom.com>
2020-09-10 12:47:01 +0200

    work

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="container">
-    <h1>Libary</h1>
-    <p>Dooim</p>
-    <!-- your html code here -->
-  </div>
+  <body>
+    <div class="container">
+      <h1>Library</h1>
+      <h2>Search</h2>
+      <h3>Audio / Video</h3>
+      <h3>Books / E-Books</h3>
+      <h3>Images</h3>
+      <h3>Magazine articles</h3>
+      <h3>Manuscripts</h3>
+      <h3>Newspapers</h3>
+      <h3>Official Publications</h3>
+      <h3>Sheet Music</h3>
+      <h3>Statistics</h3>
+      <h3>University Theses</h3>
+    </div>
+  </body>
+
 </template>
 <script src="./Home.js"></script>
 <style src="./Home.scss" lang="scss" scoped></style>
